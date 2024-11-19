d741a03fa2ed0602aa5b828cf81b265cff269bd3
gwleuverink <willem@leuver.ink>
2024-11-19 15:39:14 +0100

    wip - auth screen polish

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-// import './bootstrap';
+const { shell } = require('electron')
+
+Alpine.directive('default-browser', (el) => {
+    if(el.nodeName !== 'A') {
+        return;
+    }
+
+    el.addEventListener('click', event => {
+        event.preventDefault();
+        event.stopPropagation()
+
+        shell.openExternal(el.href);
+    });
+
+
+    return (new Date).toLocaleTimeString()
+})
--- resources/views/livewire/auth.blade.php
+++ resources/views/livewire/auth.blade.php
@@ -1,31 +1,40 @@
 <div
     wire:init="startUserVerification"
     wire:poll.keep-alive.8s="pollAuthorization"
-    class="p-4"
+    class="flex min-h-screen flex-col p-4"
 >
-    <p>By authenticating with GitHub, you can see workflows of private repos and enjoy higher rate limits.</p>
+    <div class="flex w-full flex-col items-center space-y-6 px-4 py-6">
+        <x-svg.github-mark />
 
-    <p>
-        You will need to type this code when connecting to GitHub:
-        <span>
-            @if ($userCode && $deviceCode)
-                <span class="select-all bg-blue-800 px-3 py-2 text-white">{{ $userCode }}</span>
-            @else
-                loading...
-            @endif
-        </span>
-    </p>
+        <x-input.button
+            :href="$verificationUri"
+            x-default-browser
+            target="_blank"
+        >
+            Connect with GitHub
+        </x-input.button>
+    </div>
 
-    <a
-        href="https://github.com/login/device"
-        target="_blank"
-        class="inline-block"
-    >
-        Connect to GitHub.
-    </a>
+    <div>
+        <p>Please connect with GitHub to continue.</p>
 
-    <p>
-        Copy this URL in your browser, if you can't click the link above:
-        <span class="select-all text-indigo-700 underline underline-offset-2">https://github.com/login/device</span>
-    </p>
+        <p>You will need to type this code prompted:</p>
+    </div>
+
+    <div class="mt-2">
+        @if ($userCode && $deviceCode)
+            <code class="inline-block cursor-default select-all rounded-sm bg-blue-800 px-2 py-0.5 font-mono text-xs font-semibold text-white">{{ $userCode }}</code>
+        @else
+            <span>loading...</span>
+        @endif
+    </div>
+
+    <div class="mt-auto text-xs">
+        <p>
+            Copy this URL in your browser, if you can't click the link above:
+            <span class="inline cursor-default select-all text-indigo-700 underline underline-offset-2">
+                {{ $verificationUri }}
+            </span>
+        </p>
+    </div>
 </div>
