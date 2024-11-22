90a4e065716447878707858e6c047c762f397342
gwleuverink <willem@leuver.ink>
2024-11-22 17:04:58 +0100

    add context menu button

--- resources/views/livewire/workflow-run.blade.php
+++ resources/views/livewire/workflow-run.blade.php
@@ -15,13 +15,16 @@
     };
 @endphp
 
-<article @if ($run->status->isRunning())
-    wire:poll.5s="refresh"
-@endif>
+<article
+    @if ($run->status->isRunning())
+        wire:poll.5s="refresh"
+    @endif
+    :wire:key="$run->remote_id"
+>
     <div @class([
         $colors,
-        'border-l-4 bg-neutral-50 p-3',
-        'opacity-70' => $run->status->isFinished(),
+        'group border-l-4 bg-neutral-50 p-3',
+        'opacity-70 transition-opacity hover:opacity-100' => $run->status->isFinished(),
     ])>
         <div class="mb-2 flex items-center justify-between">
             <div class="flex items-center space-x-2">
@@ -29,12 +32,10 @@
                 <div class="text-[10px] text-neutral-500">{{ '-' }}</div>
             </div>
 
-            <div class="text-[10px] text-neutral-300">{{ $run->started_at_diff }}</div>
+            <div class="text-[10px] text-neutral-400">{{ $run->started_at_diff }}</div>
         </div>
 
         <div class="space-y-2">
-            {{-- {{ $run->status->value }} --}}
-
             <div @class([
                 'flex items-center space-x-3',
             ])>
@@ -45,7 +46,16 @@
 
                 <div class="min-w-0 flex-grow">
                     <div class="truncate text-neutral-500">{{ $run->name }}</div>
-                    <div class="truncate text-[9px] text-neutral-400">{{ $run->conclusion?->value ?? $run->status->value }}</div>
+                    <div class="truncate text-[9px] text-neutral-400">{{ $run->conclusion?->forHumans() ?? $run->status->forHumans() }}</div>
+                </div>
+
+                <div>
+                    <button
+                        type="button"
+                        class="transi cursor-default rounded-full p-0.5 opacity-20 transition-all hover:bg-neutral-200 group-hover:opacity-100"
+                    >
+                        <x-heroicon-c-ellipsis-vertical class="w-4 text-neutral-500" />
+                    </button>
                 </div>
             </div>
         </div>
