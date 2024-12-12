e8d3a9bdc0ea32dd45749dc62d645840bb2b64e0
gwleuverink <willem@leuver.ink>
2024-12-12 16:29:46 +0100

    tweak schedule timing

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Schedule;
 use App\Console\Commands\FetchWorkflowRuns;
 
 Schedule::command(FetchWorkflowRuns::class)
-    ->everyFifteenSeconds()
+    ->everyTwentySeconds()
     ->runInBackground();
