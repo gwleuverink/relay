4be050224e4695b60c40a2e934be4f15cc1df57a
gwleuverink <willem@leuver.ink>
2024-12-17 01:13:41 +0100

    tidy

--- resources/views/livewire/auth.blade.php
+++ resources/views/livewire/auth.blade.php
@@ -68,40 +68,4 @@
         <div class="h-2 w-2 animate-pulse rounded-full bg-blue-600"></div>
         <span>Waiting for authentication...</span>
     </div>
-    {{--
-        <div class="flex w-full flex-col items-center space-y-6 px-4 py-6">
-        <x-svg.github-mark />
-        
-        <x-input.button
-        :href="$verificationUri"
-        x-open-external
-        target="_blank"
-        >
-        Connect with GitHub
-        </x-input.button>
-        </div>
-        
-        <div>
-        <p>Please connect with GitHub to continue.</p>
-        
-        <p>You will need to type this code prompted:</p>
-        </div>
-        
-        <div class="mt-2">
-        @if ($userCode && $deviceCode)
-        <code class="inline-block cursor-default select-all rounded-sm bg-blue-800 px-2 py-0.5 font-mono text-xs font-semibold text-white">{{ $userCode }}</code>
-        @else
-        <span>loading...</span>
-        @endif
-        </div>
-        
-        <div class="mt-auto text-xs">
-        <p>
-        Copy this URL in your browser if you can't click the link above:
-        <span class="inline cursor-default select-all text-indigo-700 underline underline-offset-2">
-        {{ $verificationUri }}
-        </span>
-        </p>
-        </div>
-    --}}
 </div>
