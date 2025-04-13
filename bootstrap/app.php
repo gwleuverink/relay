b32c33cf45cabde2f42779f55e6ab7f3f0c45d43
gwleuverink <willem@leuver.ink>
2025-04-13 16:01:50 +0200

    dont apply middleware when running unit tests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(PreventRegularBrowserAccess::class);
+        if (! app()->runningUnitTests()) {
+            $middleware->append(PreventRegularBrowserAccess::class);
+        }
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
